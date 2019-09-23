387d6ada1315b4705dc1b616e3de170108c14eb1
AnMihyang <cmhh0808@naver.com>
2019-09-23 17:54:35 +0900

    주석 수정

--- Echo_Client_loop_0918/src/Echo_Client_loop_0918.cpp
+++ Echo_Client_loop_0918/src/Echo_Client_loop_0918.cpp
@@ -6,7 +6,7 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-// ----Client 명령 랜덤 반복----
+// ----Client 명령 랜덤 반복(Echo_Server_0918 프로젝트와 통신)----
 
 #include "PacketDefine.h"
 #include <iostream>
